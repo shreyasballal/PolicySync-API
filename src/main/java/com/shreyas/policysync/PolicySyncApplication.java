d5c42c1aee165d7f67385b1402faf89cd104d5b3
Shreyas Ballal <shreyasballal5@gmail.com>
2025-04-29 15:56:09 +0530

    Initial Spring Boot scaffold with SQLite and dialect fix

---
+++ src/main/java/com/shreyas/policysync/PolicySyncApplication.java
@@ -0,0 +1,13 @@
+package com.shreyas.policysync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicySyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicySyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shreyas/policysync/PolicySyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreyas.policysync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicySyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
